fad236f12156b4fea3ac482e30f2314689068b24
Ishan <Braviaspan2002@gmail.com>
2024-05-26 01:44:21 +0530

    Added product modal

---
+++ models/Product.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { Schema } from "mongoose";
+
+const ProductSchema = new Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    hsn_code: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+
+})
+
+const Product = mongoose.model('product', ProductSchema)
+
+export default Product
