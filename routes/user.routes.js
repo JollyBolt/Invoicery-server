7df58233632aaf799461b7b6301472f6ebe20d0c
Ishan <Braviaspan2002@gmail.com>
2024-06-11 00:06:14 +0530

    Created user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,30 +2,22 @@ import { Router } from "express"
 import {
   getUser,
   updateUser,
+  createUser,
+  // deleteUser,
 } from "../controllers/user.controller.js"
 import { body } from "express-validator"
 import { verifyToken } from "../middleware/verifyToken.js"
 
 const router = Router()
 
-router
-  .route("/getUser")
-  .get(
-    verifyToken,
-    getUser
-  )
+router.route("/getUser").get(verifyToken, getUser)
 
-// router.route("/signup").post(
-//   body("name", "Name must be of atleast 3 characters.").isLength({ min: 3 }),
-//   body("org", "Org must be of atleast 3 characters.").isLength({ min: 3 }),
-//   body("email", "Enter a valid Email").isEmail(),
-//   body("password", "Password must be of atleast 8 characters.").isLength({
-//     min: 8,
-//   }),
-//   signup
-// )
-
+router.route("/createUser").post(
+  body("name", "Name must be of atleast 3 characters.").isLength({ min: 3 }),
+  body("org", "Org must be of atleast 3 characters.").isLength({ min: 3 }),
+  verifyToken,
+  createUser
+)
 router.route("/updateUser/:id").put(verifyToken, updateUser)
-
 
 export default router
