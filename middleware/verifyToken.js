7dd7cd5b4c7c5cd7a4a8d2b49cd591921e808975
Ishan <Braviaspan2002@gmail.com>
2024-06-10 20:54:38 +0530

    followed industry standard

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,7 +4,7 @@ const secret = process.env.JWT_SECRET
 
 const verifyToken = async(req,res,next) => {
 
-    const token = req.header('authToken') 
+    const token = req.header('Authorization').split(' ')[1]
     if(!token) res.status(401).send("Access Denied")
 
     try{
