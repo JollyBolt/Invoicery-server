f59abb1858b8a93980ff4f42e30d85e19cd3c903
Ishan <Braviaspan2002@gmail.com>
2024-06-11 00:07:15 +0530

    Updated auth

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,30 +1,40 @@
-import User from "../models/User.js";
-import { validationResult } from "express-validator";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import UserAuth from "../models/UserAuth.js"
+import { validationResult } from "express-validator"
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET
 
 const login = async (req, res) => {
-  const errors = validationResult(req);
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array()[0].msg });
+    return res.status(400).json({ errors: errors.array()[0].msg })
   }
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email: email }); //get required user based on entered email
-    if (!user) {
-      //if user doesn't exist
-      return res.status(401).send("User with this email does not exist.");
+    const { mode, email } = req.body
+    const user = await UserAuth.findOne({ email: email }) //get required user based on entered email
+    if (mode === "password") {
+      const { password } = req.body
+      if (!user) {
+        //if user doesn't exist
+        return res.status(401).send("UserAuth with this email does not exist.")
+      }
+      if (!passwordLogin)
+        return res.status(401).send("You did not sign up using password.")
+      const passwordCompare = await bcrypt.compare(password, user.password) //comparing password(1st arg) entered and password hash stored in db(2nd arg)
+      if (!passwordCompare) {
+        //if user exists but password is wrong
+        return res.status(401).send("Wrong Password.")
+      }
+    } else if (mode === "google") {
+      if (!user.googleLogin)
+        return res.status(401).send("You did not sign up using google.")
+      if (user.googleCredentials !== googleCredentials)
+        return res.status(401).send("Wrong Credentials.")
     }
 
-    const passwordCompare = await bcrypt.compare(password, user.password); //comparing password(1st arg) entered and password hash stored in db(2nd arg)
-    if (!passwordCompare) {
-      //if user exists but password is wrong
-      return res.status(401).send("Wrong Password.");
-    }
     //if both email and password are correct and user is successfully retrieved
     const token = jwt.sign(
       {
@@ -32,55 +42,64 @@ const login = async (req, res) => {
       },
       secret,
       { expiresIn: "1d" }
-    );
-    const expiresIn = new Date();
-    expiresIn.setDate(new Date().getDate() + 1); //set expire date to 1 day later
+    )
+    const expiresIn = new Date()
+    expiresIn.setDate(new Date().getDate() + 1) //set expire date to 1 day later
     res.cookie("authToken", token, {
       expires: expiresIn,
       httpOnly: false,
       sameSite: "none",
       secure: true,
-    }); //send the token as cookie to frontend (cookie exipires 2 days later)
-    res.status(200).json({ token }); //jwt auth token is returned as json
+    }) //send the token as cookie to frontend (cookie exipires 2 days later)
+    res.status(200).json({ token }) //jwt auth token is returned as json
   } catch (error) {
     //if error related to request occurs
-    console.log(error);
-    res.status(500).send("Internal error occured");
+    console.log(error)
+    res.status(500).send("Internal error occured")
   }
-};
+}
 
 const test = async (req, res) => {
-  const expiresIn = new Date();
-  expiresIn.setDate(new Date().getDate() + 1); //set expire date to 1 day later
+  const expiresIn = new Date()
+  expiresIn.setDate(new Date().getDate() + 1) //set expire date to 1 day later
   res.cookie("authToken", "token", {
     expires: expiresIn,
     httpOnly: false,
     sameSite: "none",
     secure: true,
-  }); //send the token as cookie to frontend (cookie exipires 2 days later)
-  res.status(200).send("cookie set"); //jwt auth token is returned as json
-};
+  }) //send the token as cookie to frontend (cookie exipires 2 days later)
+  res.status(200).send("cookie set") //jwt auth token is returned as json
+}
 
 const signup = async (req, res) => {
-  const errors = validationResult(req);
+  const errors = validationResult(req)
   if (!errors.isEmpty()) {
     //error occurs due to invalid input
-    return res.status(400).json({ errors: errors.array()[0].msg });
+    return res.status(400).json({ errors: errors.array()[0].msg })
   }
   try {
-    let user = await User.findOne({ email: req.body.email }); //checking if user with email already exists
+    let user = await UserAuth.findOne({ email: req.body.email }) //checking if user with email already exists
     // console.log(user)
     if (user) {
       //handling if user with email already exists
       return res
         .status(400)
-        .json({ error: "Sorry, User with email already exists" }); //return is used so if this runs,it doesn't reach underlying code
+        .json({ error: "Sorry, UserAuth with email already exists" }) //return is used so if this runs,it doesn't reach underlying code
     }
     //create new user
-    const salt = await bcrypt.genSalt(10); //creating salt
-    const secPass = await bcrypt.hash(req.body.password, salt); //creating secure password by combining user entered paas with salt and then hashed
-    user = new User({ ...req.body, password: secPass }); //create new user from input based on User model
-    await user.save(); //save new user document to collection in DB
+
+    const { mode } = req.body
+
+    if (mode === "password") {
+      const salt = await bcrypt.genSalt(10) //creating salt
+      const secPass = await bcrypt.hash(req.body.password, salt) //creating secure password by combining user entered paas with salt and then hashed
+      user = new UserAuth({ ...req.body, password: secPass, passwordLogin: true }) //create new user from input based on UserAuth model
+    } else if (mode === "google") {
+      user = new UserAuth({ ...req.body, googleLogin: true }) //create new user from input based on UserAuth model
+    } else if (mode === "otp") {
+      user = new UserAuth({ ...req.body }) //create new user from input based on UserAuth model
+    }
+    await user.save() //save new user document to collection in DB
     const token = jwt.sign(
       //creating authtoken
       {
@@ -88,23 +107,21 @@ const signup = async (req, res) => {
       },
       secret,
       { expiresIn: "1d" }
-    );
-    const expiresIn = new Date();
-    expiresIn.setDate(new Date().getDate() + 1); //set expire date to 1 day later
+    )
+    const expiresIn = new Date()
+    expiresIn.setDate(new Date().getDate() + 1) //set expire date to 1 day later
     res.cookie("authToken", token, {
       expires: expiresIn,
       httpOnly: false,
       sameSite: "none",
       secure: true,
-    }); //send the token as cookie to frontend (cookie exipires 2 days later)
-    res.status(201).json(token);
+    }) //send the token as cookie to frontend (cookie exipires 2 days later)
+    res.status(201).json({ token, user })
   } catch (error) {
     //if error related to request occurs
-    res.status(500).send("Internal error occured");
+    res.status(500).send("Internal error occured")
     // res.status(500).json({'error':error});
   }
-};
+}
 
-
-
-export { login, signup, test };
+export { login, signup, test }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,9 @@
-import { Router } from "express";
-import {
-  login,
-  signup,
-  test,
-} from "../controllers/auth.controller.js";
-import { body } from "express-validator";
-import { verifyToken } from "../middleware/verifyToken.js";
+import { Router } from "express"
+import { login, signup, test } from "../controllers/auth.controller.js"
+import { body } from "express-validator"
+import { verifyToken } from "../middleware/verifyToken.js"
 
-const router = Router();
+const router = Router()
 
 router
   .route("/login")
@@ -15,19 +11,18 @@ router
     body("email", "Enter a valid Email").isEmail(),
     body("password", "Password can not be blank").isLength({ min: 8 }),
     login
-  );
+  )
 
 router.route("/signup").post(
-  body("name", "Name must be of atleast 3 characters.").isLength({ min: 3 }),
-  body("org", "Org must be of atleast 3 characters.").isLength({ min: 3 }),
+  // body("name", "Name must be of atleast 3 characters.").isLength({ min: 3 }),
+  // body("org", "Org must be of atleast 3 characters.").isLength({ min: 3 }),
   body("email", "Enter a valid Email").isEmail(),
-  body("password", "Password must be of atleast 8 characters.").isLength({
-    min: 8,
-  }),
+  // body("password", "Password must be of atleast 8 characters.").isLength({
+  //   min: 8,
+  // }),
   signup
-);
+)
 
+router.route("/test").get(test)
 
-router.route("/test").get(test);
-
-export default router;
+export default router
