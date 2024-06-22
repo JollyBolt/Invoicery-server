be71f97ad81a12a440b70c71a5d3d8d3acce9f37
Ishan <Braviaspan2002@gmail.com>
2024-06-22 16:09:34 +0530

    added search and pagination

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1,59 +1,65 @@
-import Customer from "../models/Customer.js";
-import dotenv from "dotenv";
-dotenv.config();
+import Customer from "../models/Customer.js"
+import dotenv from "dotenv"
+dotenv.config()
 
 const getAllCustomers = async (req, res) => {
   try {
-    const userId = req.id;
-    let customerList = await Customer.find({ userId: userId });
-    res.status(200).send(customerList);
+    const userId = req.id
+    let search = req.query.search || ""
+    const page = req.query.page || 0
+    const limit = req.query.limit || 10
+    let customerList = await Customer.find({
+      userId: userId,
+      client: { $regex: search , $options: 'i' },
+    }).skip(page*limit).limit(limit)
+    res.status(200).send(customerList)
   } catch (e) {
-    res.status(500).send("Internal server error occured");
+    res.status(500).send("Internal server error occured")
   }
-};
+}
 
 const getCustomer = async (req, res) => {
   try {
-    const customer = await Customer.findById(req.params.id);
-    res.status(200).send(customer);
+    const customer = await Customer.findById(req.params.id)
+    res.status(200).send(customer)
   } catch (e) {
-    res.status(500).send("Internal server error occured");
+    res.status(500).send("Internal server error occured")
   }
-};
+}
 
 const createCustomer = async (req, res) => {
   try {
-    const userId = req.id;
-    const customer = new Customer(req.body);
-    customer.userId = userId;
-    await customer.save();
-    res.status(201).json(customer);
+    const userId = req.id
+    const customer = new Customer(req.body)
+    customer.userId = userId
+    await customer.save()
+    res.status(201).json(customer)
   } catch (e) {
-    res.status(500).send("Internal server error occured");
+    res.status(500).send("Internal server error occured")
   }
-};
+}
 
 const editCustomer = async (req, res) => {
   try {
-    const customer = await Customer.findByIdAndUpdate(req.params.id, req.body);
-    res.status(200).send(customer);
+    const customer = await Customer.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).send(customer)
   } catch (e) {
-    res.status(500).send("Internal server error occured");
+    res.status(500).send("Internal server error occured")
   }
-};
+}
 
 const deleteCustomer = async (req, res) => {
   try {
-    const customer = await Customer.findByIdAndDelete(req.params.id);
-    res.status(200).send(customer);
+    const customer = await Customer.findByIdAndDelete(req.params.id)
+    res.status(200).send(customer)
   } catch (e) {
-    res.status(500).send("Internal server error occured");
+    res.status(500).send("Internal server error occured")
   }
-};
+}
 export {
   getAllCustomers,
   createCustomer,
   editCustomer,
   deleteCustomer,
   getCustomer,
-};
+}
