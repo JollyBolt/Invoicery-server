9953c44e76a6a3735165e25d2eb667d65430dd7c
Ishan <Braviaspan2002@gmail.com>
2024-06-26 15:32:42 +0530

    removed semicolon

--- routes/invoice.routes.js
+++ routes/invoice.routes.js
@@ -1,32 +1,29 @@
-import { Router } from "express";
-import { getAllInvoices } from "../controllers/invoice.controller.js";
-import { getSingleInvoice } from "../controllers/invoice.controller.js";
-import { createInvoice } from "../controllers/invoice.controller.js";
-import { deleteInvoice } from "../controllers/invoice.controller.js";
-import { verifyToken } from "../middleware/verifyToken.js";
-import { body } from "express-validator";
+import { Router } from "express"
+import { getAllInvoices } from "../controllers/invoice.controller.js"
+import { getSingleInvoice } from "../controllers/invoice.controller.js"
+import { createInvoice } from "../controllers/invoice.controller.js"
+import { deleteInvoice } from "../controllers/invoice.controller.js"
+import { verifyToken } from "../middleware/verifyToken.js"
 
-const router = Router();
+const router = Router()
 
 //Fetch all invoices
-router.get("/getallinvoices", verifyToken, getAllInvoices);
+router.get("/getallinvoices", verifyToken, getAllInvoices)
 
 //Fetch single invoice
-router.get("/getinvoice/:id", verifyToken, getSingleInvoice);
+router.get("/getinvoice/:id", verifyToken, getSingleInvoice)
 
 //Add Invoice
-router
-  .route("/createinvoice")
-  .post(
-    // body("email", "Enter a valid Email").isEmail(),
-    verifyToken,
-    createInvoice
-  );
+router.route("/createinvoice").post(
+  // body("email", "Enter a valid Email").isEmail(),
+  verifyToken,
+  createInvoice
+)
 
 //edit customer
-// router.put("/editcustomer/:id", verifyToken, editCustomer); 
+// router.put("/editcustomer/:id", verifyToken, editCustomer);
 
 //delete invoice
-router.delete("/deleteinvoice/:id", verifyToken, deleteInvoice);
+router.delete("/deleteinvoice/:id", verifyToken, deleteInvoice)
 
-export default router;
+export default router
