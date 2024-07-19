6abe5fdb006acb79d9215383a56796118f6d3515
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-07-20 02:56:47 +0530

    slight changes in cors origin

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ app.use(cookieParser())
 app.use(
   cors({
     credentials: true,
-    origin:"http://localhost:5173"
+    origin: "https://invoicery.ishansen.in"
   })
 )
-// origin: "https://invoicery.ishansen.in"
+// origin:"http://localhost:5173"
 app.use(express.json())
 
 app.use("/api/v1/auth", authRouter)
