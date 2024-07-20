94f188669772f8a86450cc776bd6982a181f4d86
Ishan <Braviaspan2002@gmail.com>
2024-07-20 22:41:16 +0530

    removed clg cookies

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,28 +1,24 @@
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken"
 
 const secret = process.env.JWT_SECRET
 
-const verifyToken = async(req,res,next) => {
-
-    // const token = req.headers.authorization.split(" ")[1]
-    const token = req.cookies.authToken
-    // console.log(token)
-    if(!token) res.status(401).send("Access Denied")
-    try{
-        const payload = jwt.verify(token,secret)
-        req.id = payload.id  //this id is _id of user
-        next()
-    } catch (e) {
-        console.log({
-          msg: "Error occured in verifyToken",
-          error: e.message,
-        })
-        res.status(500).send({
-          msg: "Internal server error occured",
-          error: e.message,
-        })
-    }
-    
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies.authToken
+  if (!token) res.status(401).send("Access Denied")
+  try {
+    const payload = jwt.verify(token, secret)
+    req.id = payload.id //this id is _id of user
+    next()
+  } catch (e) {
+    console.log({
+      msg: "Error occured in verifyToken",
+      error: e.message,
+    })
+    res.status(500).send({
+      msg: "Internal server error occured",
+      error: e.message,
+    })
+  }
 }
 
 export { verifyToken }
