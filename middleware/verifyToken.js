d1a5d4b175342301ce24d8704b053ed999146bfc
Robin Sen <robinsen@Robins-MacBook-Air.local>
2024-07-20 19:35:53 +0530

    changing authToken verification logic

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,7 +4,8 @@ const secret = process.env.JWT_SECRET
 
 const verifyToken = async(req,res,next) => {
 
-    const token = req.headers.authorization.split(" ")[1]
+    // const token = req.headers.authorization.split(" ")[1]
+    const token = req.cookies.authToken
     // console.log(token)
     if(!token) res.status(401).send("Access Denied")
     try{
